3d76bed6d27a2841f6e36ef06b423adea89f8b89
CKtoo <christian.klemenschitz@gmail.com>
2020-01-24 10:10:02 +0100

    Erweiterung auf 2 Inputs

--- SOFAlizer~/SOFAlizer~.c
+++ SOFAlizer~/SOFAlizer~.c
@@ -38,7 +38,8 @@ typedef struct _SOFAlizer_tilde
     t_float leftIR[128]; 		  /* holds left impulse response */
 	t_float rightIR[128];		  /* holds right impulse response */
 
-    t_float convBuffer[128];	  /* convolution buffer */
+    t_float convBuffer1[128];	  /* convolution buffer */
+    t_float convBuffer2[128];	  /* convolution buffer */
     t_float f ;                   /* dummy float for dsp */
     t_int bufferPin;			  /* indexing in convolution buffer */ 
 } t_SOFAlizer_tilde;
@@ -46,10 +47,11 @@ typedef struct _SOFAlizer_tilde
 static t_int *SOFAlizer_tilde_perform(t_int *w)
 {
     t_SOFAlizer_tilde *x = (t_SOFAlizer_tilde *)(w[1]) ;
-    t_float *in = (t_float *)(w[2]);
-    t_float *right_out = (t_float *)(w[3]);
-    t_float *left_out = (t_float *)(w[4]); 
-    int blocksize = (int)(w[5]);
+    t_float *in1 = (t_float *)(w[2]);
+    t_float *in2 = (t_float *)(w[3]);
+    t_float *right_out = (t_float *)(w[4]);
+    t_float *left_out = (t_float *)(w[5]); 
+    int blocksize = (int)(w[6]);
 
     x->azi = x->azimuth ; 
     x->ele = x->elevation ;
@@ -62,43 +64,51 @@ static t_int *SOFAlizer_tilde_perform(t_int *w)
 	x->values[1] = x->rad * cos(Pi/180 * x->ele) * sin(Pi/180 * x->azi);
 	x->values[2] = x->rad * sin(Pi/180 * x->ele);
 	
-	int nearest;
+	int nearest, cnt = 0;
 	int size = x->N * x->hrtf->hrtf->R;
 	nearest = mysofa_lookup(x->hrtf->lookup, x->values);
+	if (cnt > 3000){ post("index = %u", nearest); cnt = 0; };
+	cnt+=1;
 	
 	for (i = 0; i < 128; i++) {
 		x->leftIR[i] = x->hrtf->hrtf->DataIR.values[nearest * size + i];
 		x->rightIR[i] = x->hrtf->hrtf->DataIR.values[nearest * size + x->N + i];
 	}
 
-    float inSample;
-	float convSum[2]; // to accumulate the sum during convolution.
-	
+    float inSample1, inSample2;
+	float convSum1[2]; // to accumulate the sum during convolution.
+	float convSum2[2];
 	// Convolution algorithm
 	while (blocksize--) {		
-		convSum[0] = 0; 
-		convSum[1] = 0;	
-		inSample = *(in++);
-		x->convBuffer[x->bufferPin] = inSample;
+		convSum1[0] = 0; 
+		convSum1[1] = 0;
+		convSum2[0] = 0; 
+		convSum2[1] = 0;	
+		inSample1 = *(in1++);
+		inSample2 = *(in2++);
+		x->convBuffer1[x->bufferPin] = inSample1; 
+		x->convBuffer2[x->bufferPin] = inSample2; 
 			
 		for (n = 0; n < 128; n++) { 
-			convSum[0] += x->leftIR[n] * x->convBuffer[(x->bufferPin-n) &127];
-			convSum[1] += x->rightIR[n] * x->convBuffer[(x->bufferPin-n) &127];
+			convSum1[0] += x->leftIR[n] * x->convBuffer1[(x->bufferPin-n) &127];
+			convSum1[1] += x->rightIR[n] * x->convBuffer1[(x->bufferPin-n) &127];
+			convSum2[0] += x->leftIR[n] * x->convBuffer2[(x->bufferPin-n) &127];
+			convSum2[1] += x->rightIR[n] * x->convBuffer2[(x->bufferPin-n) &127];
 		}	
 		  
-        *left_out++ = convSum[0];
-     	*right_out++ = convSum[1];
+        *left_out++ = (convSum1[0] + convSum2[0]) / 2 ;
+     	*right_out++ = (convSum1[1] + convSum2[1]) / 2;
      	
      	x->bufferPin = (x->bufferPin + 1) & 127;
     }
 	
-    return (w+6);
+    return (w+7);
 }
 
 static void SOFAlizer_tilde_dsp(t_SOFAlizer_tilde *x, t_signal **sp)
 {
 		// callback, params, userdata, in_samples, out_L,		out_R,		blocksize.
-    dsp_add(SOFAlizer_tilde_perform, 5, x,  sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[0]->s_n);
+    dsp_add(SOFAlizer_tilde_perform, 6, x,  sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[0]->s_n);
 }
 
 void SOFAlizer_tilde_free(t_SOFAlizer_tilde *x)
@@ -115,10 +125,11 @@ static void *SOFAlizer_tilde_new(t_symbol *filenameArg, t_floatarg azimArg, t_fl
 		return 0;
 	}
 	else {
-		post("SOFA file %s will be opend", x->filename);
+		post("SOFA file %s will be opened..", x->filename);
 	}
     x->left_channel = outlet_new(&x->x_obj, gensym("signal"));
     x->right_channel = outlet_new(&x->x_obj, gensym("signal"));
+    inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal,&s_signal); //audio input 2
     floatinlet_new(&x->x_obj, &x->azimuth) ;     /* 0 to 360 degrees */
     floatinlet_new(&x->x_obj, &x->elevation) ;   /* -90 to 90 degrees */
 	
@@ -130,15 +141,15 @@ static void *SOFAlizer_tilde_new(t_symbol *filenameArg, t_floatarg azimArg, t_fl
     // Read in SOFA file
     x->hrtf = mysofa_open(x->filename, 44100, &filter_length, &err);
     if(x->hrtf == NULL) {
-		error("SOFA file couldn't be opened");
+		error("SOFA file couldn't be opened.");
 		return 0;
 	}
 	       
     x->M = x->hrtf->hrtf->M;
 	x->N = x->hrtf->hrtf->N;
 	
-	fprintf(stderr, "mit_kemar_normal_pinna.sofa: %u HRTFs, %u samples @ %u Hz. %s, %s, %s.\n",
-							(unsigned int)x->M, (unsigned int)x->N,
+	post("Metadata: %s %u HRTFs, %u samples @ %u Hz. %s, %s, %s.\n",
+							x->filename,(unsigned int)x->M, (unsigned int)x->N,
 							(unsigned int)(x->hrtf->hrtf->DataSamplingRate.values[0]),
 							mysofa_getAttribute(x->hrtf->hrtf->attributes, "DatabaseName"),
 							mysofa_getAttribute(x->hrtf->hrtf->attributes, "Title"),
@@ -153,7 +164,7 @@ static void *SOFAlizer_tilde_new(t_symbol *filenameArg, t_floatarg azimArg, t_fl
 			fprintf(stderr, " delay = %f", delay);
 		}
 	}	
-	if (warn == 1) post(" Warning: This SOFA file will be processed wrong besause of IR delays!");		 
+	if (warn == 1){ error(" Warning: This SOFA file will be processed wrong besause of IR delays!"); }		 
     
     post("        SOFAlizer~: binaural filter with measured reponses\n") ;
     post("        elevation: -90 to 90 degrees. azimuth: 360") ;
@@ -164,7 +175,8 @@ static void *SOFAlizer_tilde_new(t_symbol *filenameArg, t_floatarg azimArg, t_fl
 	}
         
     for (i = 0; i < 128 ; i++) {
-         x->convBuffer[i] = 0; 			  // initialise convolution buffer
+         x->convBuffer1[i] = 0; 			  // initialise convolution buffer 1
+         x->convBuffer2[i] = 0; 			  // initialise convolution buffer 2
     }
 	
     x->bufferPin = 0;					  // initialise index of signal for convolution
